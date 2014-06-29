e29e0a5feb8f17d597fce7546eaf12eaba2e5b04
Chau Nguyen <vo.tri.thong@gmail.com>
2014-06-29 11:35:27 +0700

    1st MATLAB commit, work with the below text output from serial port. Just need to run setupSerial function x33055y32767z49564

---
+++ Tutorial 2/calibrate.m
@@ -0,0 +1,39 @@
+function calCo= calibrate(s)
+    out.s=s;
+    calCo.offset=0;
+    calCo.g=1;
+      %read push the raw accelerometer output at three different orientations
+      mbox = msgbox('Lay accelerometer on a flat surface.', 'Calibration'); uiwait(mbox);
+      flushinput(s);
+      [gx_z gy_z gz_z] = readAcc(out,calCo);   
+	  %gZ = 1, gX = gY = 0 orientation
+      
+	  mbox = msgbox('Stand accelerometer on edge so that X arrow points up.', 'Calibration'); uiwait(mbox); 
+      flushinput(s);
+      [gx_x gy_x gz_x] = readAcc(out,calCo);   %gX = 1, gY = gZ = 0 orientation
+      
+	  mbox = msgbox('Stand accelerometer on edge so that Y arrow points up.', 'Calibration'); uiwait(mbox);
+      flushinput(s);
+      [gx_y gy_y gz_y] = readAcc(out,calCo);   %gY = 1, gX = gZ = 0 orientation
+      
+	  %calculate offsets for each axis
+      offsetX = (gx_z + gx_y) / 2;
+      offsetY = (gy_x + gy_z) / 2;
+      offsetZ = (gz_x + gz_y) / 2;
+    
+	 %calculate scaling factors compare to 1g.
+	 
+      gainX = gx_x - offsetX;
+      gainY = gy_y - offsetY;
+      gainZ = gz_z - offsetZ;
+      disp('gx gy gz');
+      disp(gx_x);
+      disp(gy_y);
+      disp(gz_z);
+	  calCo.offset = [offsetX offsetY offsetZ];
+      calCo.g = [gainX gainY gainZ];
+       disp('offset');
+     disp(calCo.offset);
+     disp(calCo.g);
+     
+      mbox = msgbox('Sensor calibration complete'); uiwait(mbox);
---
+++ Tutorial 2/setupSerial.m
@@ -0,0 +1,41 @@
+% ~~~~~~~~~ SERIALPUSH SETUP ~~~~~~~~~ %
+function [s,flag] = setupSerial(comPort)
+% Initialize the serialpush port communication between Arduino and MATLAB
+% The input value is the COMPORT should be changed as per requirement
+% We ensure that the arduino is also communicatiing with MATLAB at this
+% time. A predefined code on the arduino acknowledges this. 
+% if setup is complete then the value of setup is returned as 1 else 0
+
+%instrfind
+flag = 1;
+s = serial(comPort);
+set(s,'DataBits', 8 );
+set(s,'StopBits', 1 );
+set(s,'BaudRate', 9600);
+set(s,'Parity', 'none'); 
+
+fopen(s);
+
+%{
+a='b';
+while (a ~='a')
+    a=fread(s,1,'uchar');
+end
+if (a=='a')
+    disp('serial read');
+end
+
+
+fprintf(s,'%c','a');
+
+  mbox = msgbox('Serial Communication setup.'); uiwait(mbox);
+%}
+  disp(fscanf(s,'%s'));
+  %readAcc(s);
+  calibrate(s);
+if ~isempty(instrfind)
+    fclose(instrfind);
+    delete(instrfind);
+end
+close all
+  
